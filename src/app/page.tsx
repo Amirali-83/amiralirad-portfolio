7c4fbac6d48acdc00f906c25adcfe03ddb719e3e
Amirali <amirali.sotoudeh@yahoo.com>
2025-10-11 22:05:16 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -80,7 +80,7 @@ export default function Home() {
                     />
                   </a>
                 </div>
-              </div>
+            
             </div>
 
             {/* Right column */}
